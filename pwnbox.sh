9dd6fbbb530cee0bd8ae463d50de848a7eeae6f9
Georgiy Balashov <hwrryss@gmail.com>
2025-01-14 16:37:31 +0300

    fix: folder path

--- pwnbox.sh
+++ pwnbox.sh
@@ -2,8 +2,11 @@
 
 filename="$1"
 
+contname=$(echo "$filename" | tr "/" "-" | tr -d ".")
+
+
 if [ "$#" -lt 1 ]; then
     echo "usage: pwnbox filename/folder"
 else
-    docker run --privileged --cap-add "SYS_PTRACE" --name "pwnbox-$filename" -v "$PWD/$filename:/box/$filename"  -it "pwnbox" "/bin/zsh"
+    docker run --privileged --cap-add "SYS_PTRACE" --name "pwnbox-$contname" -v "$PWD/$filename:/box/$filename"  -it "pwnbox" "/bin/zsh"
 fi
