bb3733e8a93de80257437dd5f31e49298f371b55
Mikhail Reznichenko <justmarfix@vk.com>
2025-03-01 21:05:35 +0300

    add option

--- pwnbox.sh
+++ pwnbox.sh
@@ -7,6 +7,8 @@ sanename=$(echo "$filename" | rev | cut -d'/' -f 1 | rev)
 
 if [ "$#" -lt 1 ]; then
     echo "usage: pwnbox filename/folder"
+elif [[ "$2" == "--rm" ]]; then
+    docker run --rm --privileged --cap-add "SYS_PTRACE" --name "pwnbox-$contname" -v "$PWD/$filename:/box/$sanename" -it "pwnbox" "/bin/zsh"
 else
-    docker run --rm --privileged --cap-add "SYS_PTRACE" --name "pwnbox-$contname" --mount type=bind,source="$PWD/$filename",target="/box/$sanename"  -it "pwnbox" "/bin/zsh"
+    docker run --privileged --cap-add "SYS_PTRACE" --name "pwnbox-$contname" -v "$PWD/$filename:/box/$sanename" -it "pwnbox" "/bin/zsh"
 fi
