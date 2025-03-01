c72755a77b6f5d34c8b06111f5df5ae09af88b12
Mikhail Reznichenko <justmarfix@vk.com>
2025-03-01 20:37:45 +0300

    fix filename

--- pwnbox.sh
+++ pwnbox.sh
@@ -3,10 +3,10 @@
 filename="$1"
 
 contname=$(echo "$filename" | tr "/" "-" | tr -d ".")
-
+sanename=$(echo "$filename" | rev | cut -d'/' -f 1 | rev)
 
 if [ "$#" -lt 1 ]; then
     echo "usage: pwnbox filename/folder"
 else
-    docker run --privileged --cap-add "SYS_PTRACE" --name "pwnbox-$contname" -v "$PWD/$filename:/box/$filename"  -it "pwnbox" "/bin/zsh"
+    docker run --rm --privileged --cap-add "SYS_PTRACE" --name "pwnbox-$contname" --mount type=bind,source="$PWD/$filename",target="/box/$sanename"  -it "pwnbox" "/bin/zsh"
 fi
